2e46689abe15038ca712f003ae7321ffd28d6c6f
JoyAnna Walgren <joywalgren@gmail.com>
2025-09-03 09:47:12 -0600

    point

--- Labs/Lab2/point.cpp
+++ Labs/Lab2/point.cpp
@@ -1,8 +1,13 @@
 #include "point.h"
 
+using namespace std;
+
 //Constructor
 points::Point::Point(int X, int Y, points::Point* closestPoint)
 {
+    x = X;
+    y = Y;
+    nearestPoint = closestPoint;
 
 }
 //Destructor
@@ -14,41 +19,61 @@ points::Point::~Point()
 //Getters
 int points::Point::getX()
 {
-    return 0;
+    return x;
 }
 
 int points::Point::getY()
 {
-    return 0;
+    return y;
 }
 
 points::Point* points::Point::getNearestPoint()
 {
-    return nullptr;
+    return nearestPoint;
 }
 
 points::Point* points::Point::calcNearestPoint(Point* pointList[], unsigned long arrSize)
 {
-    return nullptr;
+    if (arrSize == 0) return nullptr;
+    
+    Point* best = nullptr;
+    double bestDist = 0.0;
+
+    for (unsigned long i = 0; i < arrSize; i++)
+    {
+        if (pointList[i] == nullptr) continue;
+
+
+        double d = distPoints(*pointList[i]);
+        if (best == nullptr || d < bestDist)
+        {
+            best = pointList[i];
+            bestDist = d;
+        }
+    }
+    return best;
+
 }
 
 //Setters
 void points::Point::setX(int newVal)
 {
-
+    x = newVal;
 }
 
 void points::Point::setY(int newVal)
 {
-
+    y = newVal;
 }
 
 void points::Point::setNearestPoint(Point* newNearestPoint)
 {
-
+    nearestPoint = newNearestPoint;
 }
 
 double points::Point::distPoints(Point& point)
 {
-    return 0.0;
+    double distanceX = x-point.getX();
+    double distanceY = y-point.getY();
+    return sqrt(distanceX*distanceX+distanceY*distanceY);
 }
